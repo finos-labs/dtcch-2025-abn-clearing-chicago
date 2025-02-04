a8e3300bb34a66eefcd854beb0c57c5e36107a95
umbraerror <easmith163@gmail.com>
2025-02-04 09:46:25 -0600

    Add LangChain AWS Bedrock model example
    - Add hello world example using AWS bedrock model
    
    Signed-off-by: umbraerror <easmith163@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+from langchain_aws import ChatBedrock
+
+model = ChatBedrock(model="anthropic.claude-3-5-sonnet-20240620-v1:0",
+    beta_use_converse_api=True)
+
+model.invoke("Hello, world!")
